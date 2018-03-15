87cd899b362b297b8e3072c21a087a979db5f3c5
Kotatko <vahalova@gmail.com>
2018-03-15 14:25:18 +0100

    promazani

--- reacter.py
+++ reacter.py
@@ -1,22 +1,13 @@
 from flask import Flask, url_for, render_template, request, redirect
-
 import requests
+
 app = Flask(__name__)
 
 URL = 'https://api.github.com/repos/{}/{}/issues/comments/{}/reactions'
 
-@app.template_filter("cap")
-def capitalize(word):
-    return word[0].upper() + word[1:]
-
 @app.route('/')
-def index(): #pohledová funkce
+def index(): 
     return render_template("reacter.html")
-
-@app.route("/hello/")
-@app.route("/hello/<name>/<int:count>/")
-def hello(name=None,count=1):
-    return render_template("reacter.html", name=name)
 
 @app.route("/reacter/", methods=['POST'])
 def url():
@@ -24,7 +15,7 @@ def url():
     repository_name = request.form['repository_name']
     id_num = request.form['id']
     url = URL.format(owner,repository_name,id_num)
-    response_for_status(url)
+    raise_for_status(url)
     return redirect(url_for("index"))
     
 
@@ -37,7 +28,7 @@ def get_session():
     session.headers.update(headers)
     return session
 
-def response_for_status(url):
+def raise_for_status(url):
     session = get_session()
     response = session.post(url,
     headers={'Accept': 'application/vnd.github.squirrel-girl-preview+jso'},
